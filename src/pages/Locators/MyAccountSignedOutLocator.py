5f02538827d46ea96ef4c576eed6236c78016524
bojan.stanisin <bzurix@gmail.com>
2022-09-15 22:59:33 +0200

    folders added

---
+++ src/pages/Locators/MyAccountSignedOutLocator.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+class MyAccountSignedOutLocator:
+
+     LOGIN_USER_NAME =(By.CSS_SELECTOR, "#username")
+     LOGIN_PASSWORD=(By.CSS_SELECTOR,"#password")
+     LOGIN_BUTTON=(By.CSS_SELECTOR,"#customer_login > div.u-column1.col-1 > form > p:nth-child(3) > button")
+
+
+
+
+
+
+
+
+
