9f11e2c16fb8d2f9f67b503146bfcd84224ec3b8
bojan.stanisin <bzurix@gmail.com>
2022-09-25 16:33:06 +0200

    adding new tests

--- Tests/My_account/test_register_new_user.py
+++ Tests/My_account/test_register_new_user.py
@@ -2,6 +2,7 @@ import pdb
 from src.helpers.generic_helpers import generate_random_email
 import pytest
 from src.pages.MyAccountSignedOut import MyAccountSignedOut
+from src.pages.MyAccountSignedIn import MyAccountSignedIn
 
 
 @pytest.mark.usefixtures("init_driver")
@@ -10,9 +11,11 @@ class TestRegisterNewUser:
     def test_register_new_user(self):
         my_account = MyAccountSignedOut(self.driver)
         my_account.go_to_my_account()
+        my_account_signed_in = MyAccountSignedIn(self.driver)
+
         random_email = generate_random_email()
         my_account.input_register_email(random_email["email"])
         my_account.input_register_password("whatareyoudoing993.")
         my_account.click_register_button()
-        pdb.set_trace()
+        my_account_signed_in.verify_user_is_signed_in()
 
---
+++ Tests/end_to_end/test_end_to_end_checkout_guest_user.py
@@ -0,0 +1,25 @@
+import pytest
+
+@pytest.mark.usefixtures("init_driver")
+class TestEndToEndCheckOutGuestUser:
+
+    def test_end_to_end_checkout_guest_user(self):
+        # go to homepage
+        # add 1 item to cart
+        # apply free coupon
+        # select free shipping
+        # click on place order
+        # verify order is received
+        # verify taht the order is recorded in database
+
+
+
+
+
+
+
+
+
+
+
+
